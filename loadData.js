ef989a57227c4423b1bb911d9a038654ab60910f
Mina Mansour <mansourmina10@gmail.com>
2021-06-16 22:49:14 +0200

    load dogs.json

--- loadData.js
+++ loadData.js
@@ -1,12 +1,12 @@
 require('./db/connect');
 require('colors');
 const { Dog } = require('./model/schemas.js');
+const dogs = require('./dogs');
 
 (async () => {
   try {
-    const p1 = Dog.create({ name: 'Bello', born: '2020-12-01' });
-    const p2 = Dog.create({ name: 'Beisser', born: '2020-12-02' });
-    await Promise.all([p1, p2]);
+    const dog = Dog.create(dogs);
+    await Promise.all([dog]);
     console.log('Data loaded');
   } catch (error) {
     console.error(`Error => ${error.message.red}`);
@@ -14,3 +14,16 @@ const { Dog } = require('./model/schemas.js');
     process.exit('SIGINT');
   }
 })();
+
+// (async () => {
+//   try {
+//     const p1 = Dog.create({ name: 'Bello', born: '2020-12-01' });
+//     const p2 = Dog.create({ name: 'Beisser', born: '2020-12-02' });
+//     await Promise.all([p1, p2]);
+//     console.log('Data loaded');
+//   } catch (error) {
+//     console.error(`Error => ${error.message.red}`);
+//   } finally {
+//     process.exit('SIGINT');
+//   }
+// })();
